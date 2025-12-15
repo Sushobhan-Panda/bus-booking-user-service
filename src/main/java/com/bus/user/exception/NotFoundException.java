159e747b26f16eecd776461b2e33bbd2f38192f1
Sushobhan-Panda <sushobhanpanda34@gmail.com>
2025-12-15 13:32:48 +0530

    Added Comments

--- src/main/java/com/bus/user/exception/NotFoundException.java
+++ src/main/java/com/bus/user/exception/NotFoundException.java
@@ -1,5 +1,6 @@
 package com.bus.user.exception;
 
+//This is a custom exception class
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
